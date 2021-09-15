cedb2f381bd7c0ddf5179c1406e79480fdd702ec
mhsnalm <alam.mohsin@gmail.com>
2021-09-15 17:22:25 +0500

    worker service

--- ProductGrpc/Startup.cs
+++ ProductGrpc/Startup.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -6,10 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ProductGrpc.Data;
 using ProductGrpc.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProductGrpc
 {
@@ -19,9 +20,12 @@ namespace ProductGrpc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
             services.AddDbContext<ProductsContext>(options =>
-                options.UseInMemoryDatabase("Products"));
+                    options.UseInMemoryDatabase("Products"));
+
+            services.AddGrpc();
+
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
