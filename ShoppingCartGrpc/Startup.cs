aa51961119c60dd730785a72bc0850c6da4b96fc
mhsnalm <alam.mohsin@gmail.com>
2021-09-16 17:59:38 +0500

    SC Worker service to begin

---
+++ ShoppingCartGrpc/Startup.cs
@@ -0,0 +1,61 @@
+﻿using DiscountGrpc.Protos;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Services;
+using System;
+
+namespace ShoppingCartGrpc
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+            services.AddDbContext<ShoppingCartContext>(options =>
+                    options.UseInMemoryDatabase("ShoppingCart"));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+                (o => o.Address = new Uri(Configuration["GrpcConfigs:DiscountUrl"]));
+
+            services.AddScoped<DiscountService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapGrpcService<GreeterService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
